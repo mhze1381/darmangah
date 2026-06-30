9d0ea94c2c29f6348c50662213467101a0d54e03
mhze1381 <mohammad.mohsan95@gmail.com>
2026-06-30 17:45:50 +0330

    Update requirement.py

--- requirement.py
+++ requirement.py
@@ -2,3 +2,13 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+with app.app_context():
+    db.create_all()
+
+    if not User.query.filter_by(username="darmangeh").first():
+        user = User(
+            username="darmangeh",
+            password="12345678"
+        )
+        db.session.add(user)
+        db.session.commit()
